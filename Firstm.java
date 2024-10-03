1e3e83b395841a67a116672675c97aa3e024e67e
chaman <chamanshekhawat712@gmail.com>
2024-10-03 11:57:17 +0530

    Initial commit

---
+++ Firstm.java
@@ -0,0 +1,72 @@
+/*public class Method {
+    void add (){
+        int a=8;
+        int b=8;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+        Method a=new Method();
+        a.add();
+
+    }
+    
+}*/
+
+public class Firstm{
+    void add(){
+        int a=4;
+        int b=4;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+
+    void div(){
+        int a=4;
+        int b=24;
+        int c=a/b;
+        System.out.println(c);
+
+    }
+
+    void multi(){
+
+        int a=4;
+        int b=6;
+        int c=a*b;
+        System.out.println(c);
+    }
+    void sub(){
+        int a=3;
+        int b=7;
+        int c=a-b;
+        System.out.println(c);
+
+    }
+    void mod(){
+        int a=4;
+        int b=12;
+        int c=a%b;
+        System.out.println(c);
+
+    }
+    public static void main(String[] args){
+        Firstm k=new Firstm();
+        k.add();
+        Firstm k1=new Firstm();
+        k1.sub();
+        Firstm k2=new Firstm();
+        k2.div();
+        Firstm k3=new Firstm();
+        k3.multi();
+        Firstm k4=new Firstm();
+        k4.sub();
+        Firstm k5=new Firstm();
+        k5.mod();
+        
+        
+
+
+    }
+}
