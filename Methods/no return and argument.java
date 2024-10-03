617bc28f039987f8df918181c23087e6ec4b0764
chaman <chamanshekhawat712@gmail.com>
2024-10-03 13:10:44 +0530

    Initial commit

---
+++ Methods/no return and argument.java
@@ -0,0 +1,144 @@
+
+/*public class Secondm {
+
+    void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+
+    }
+    void div (int a, int b){
+        int c=a/b;
+        System.out.println(c);
+
+    }
+    void multi(int a, int b){
+        int c=a*b;
+        System.out.println(c);
+
+    }
+    void sub(int a, int b){
+        int c=a-b;
+        System.out.println(c);
+
+
+    }
+    void mod(int a, int b){
+        int c=a%b;
+        System.out.println(c);
+    
+    }
+    public static void main(String[] args){
+        Secondm k=new Secondm();
+        k.add(2,2);
+        k.div(60,2);
+        k.multi(5,6);
+        k.sub(8,3);
+        k.mod(45,23);
+
+
+
+    }
+    
+}
+
+
+class A{
+    void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+
+    }
+    void div (int a, int b){
+        int c=a/b;
+        System.out.println(c);
+
+    }
+    void multi(int a, int b){
+        int c=a*b;
+        System.out.println(c);
+
+    }
+    void sub(int a, int b){
+        int c=a-b;
+        System.out.println(c);
+
+
+    }
+    void mod(int a, int b){
+        int c=a%b;
+        System.out.println(c);
+    
+    }
+
+}
+public class Secondm{
+    public static void main(String[] args){
+        A k=new A();
+        k.add(24,45);
+        k.div(67,23);
+        k.multi(33,33);
+        k.sub(22,12);
+        k.mod(34,22);
+
+    }
+}
+
+
+class A{
+    void add(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+
+    }
+
+
+}
+class B{
+    void div (int a, int b){
+        int c=a/b;
+        System.out.println(c);
+
+    }
+
+}
+class C{
+    void multi(int a, int b){
+        int c=a*b;
+        System.out.println(c);
+
+    }
+
+}
+class D{
+    void sub(int a, int b){
+        int c=a-b;
+        System.out.println(c);
+
+
+    }
+
+}
+class E{
+    void mod(int a, int b){
+        int c=a%b;
+        System.out.println(c);
+    
+    }
+
+}
+public class Secondm{
+    public static void main(String[] args){
+        A k=new A();
+        k.add(2,2);
+        B k1=new B();
+        k1.div(23,11);
+        C k2=new C();
+        k2.multi(3,4);
+        D k3=new D();
+        k3.sub(9,7);
+        E k4=new E();
+        k4.mod(33,22);
+
+
+    }
+}*/
