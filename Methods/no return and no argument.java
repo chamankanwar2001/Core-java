6d92e5f2f3f734041db97461cea6c4243307fdb7
chaman <chamanshekhawat712@gmail.com>
2024-10-03 12:43:29 +0530

    Initial commit

---
+++ Methods/no return and no argument.java
@@ -0,0 +1,183 @@
+/*public class Method {
+    void add (){
+        int a=8;
+        int b=8;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+        Method a=new Method();
+        a.add();
+
+    }
+    
+}
+
+public class Firstm{
+    void add(){
+        int a=4;
+        int b=4;
+        int c=a+b;
+        System.out.println(c);
+
+    }
+
+    void div(){
+        int a=4;
+        int b=24;
+        int c=a/b;
+        System.out.println(c);
+
+    }
+
+    void multi(){
+
+        int a=4;
+        int b=6;
+        int c=a*b;
+        System.out.println(c);
+    }
+    void sub(){
+        int a=3;
+        int b=7;
+        int c=a-b;
+        System.out.println(c);
+
+    }
+    void mod(){
+        int a=4;
+        int b=12;
+        int c=a%b;
+        System.out.println(c);
+
+    }
+    public static void main(String[] args){
+        Firstm k=new Firstm();
+        k.add();
+        Firstm k1=new Firstm();
+        k1.sub();
+        Firstm k2=new Firstm();
+        k2.div();
+        Firstm k3=new Firstm();
+        k3.multi();
+        Firstm k4=new Firstm();
+        k4.sub();
+        Firstm k5=new Firstm();
+        k5.mod();
+
+        
+
+
+    }
+}
+
+class A{
+    void add(){
+        int a=3;
+        int b=7;
+        int c=a+b;
+        System.out.println(c);
+    }
+
+    void div(){
+        int a=5;
+        int b=25;
+        int c=a/b;
+        System.out.println(c);
+
+    }
+    void multi(){
+        int a=9;
+        int b=7;
+        int c=a*b;
+        System.out.println(c);
+    }
+
+    void sub(){
+        int a=8;
+        int b=3;
+        int c=a-b;
+        System.out.println(c);
+    }
+    void mod(){
+        int a=20;
+        int b=5;
+        int c=a%b;
+        System.out.println(c);
+    }
+}
+public class Firstm{
+    public static void main(String[] args){
+        A p=new A();
+        p.add();
+        p.div();
+        p.multi();
+        p.sub();
+        p.mod();
+
+    }
+}
+
+
+
+class A{
+    void add(){
+    int a=3;
+    int b=5;
+    int c=a+b;
+    System.out.println(c);
+    }
+}
+
+class B{
+    void div(){
+    int a=5;
+    int b=2;
+    int c=a/b;
+    System.out.println(c);
+    }
+}
+
+class C{
+    void multi(){
+    int a=6;
+    int b=4;
+    int c=a*b;
+    System.out.println(c);
+    }
+}
+
+class D{
+    void sub(){
+    int a=3;
+    int b=1;
+    int c=a-b;
+    System.out.println(c);
+    }
+
+}
+class E{
+    void mod(){
+    int a=8;
+    int b=4;
+    int c=a%b;
+    System.out.println(c);
+    }
+}
+public class Firstm{
+    public static void main(String[] args){
+        A k=new A();
+        k.add();
+        B k1=new B();
+        k1.div();
+        C k2=new C();
+        k2.multi();
+        D k3=new D();
+        k3.sub();
+        E k4=new E();
+        k4.mod();
+
+
+    }
+}*/
+
