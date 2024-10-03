ef1163c6a1f77c092e04042b46e3fc9895ef39c6
chaman <chamanshekhawat712@gmail.com>
2024-10-03 11:06:14 +0530

    Initial commit

--- Method.java
+++ Method.java
@@ -1,17 +1,14 @@
-public class Firstm{
-    void add(){
-        int a=4;
+public class Method {
+    void add (){
+        int a=8;
         int b=8;
         int c=a+b;
         System.out.println(c);
     }
     public static void main(String[] args){
-
-        Firstm a=new Firstm();
+        Method a=new Method();
         a.add();
-        
-       
-
 
     }
+    
 }
