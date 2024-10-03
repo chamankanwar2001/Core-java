605eab829cb9304682563965ef7e01bb9003d0b1
chaman <chamanshekhawat712@gmail.com>
2024-10-03 10:48:13 +0530

    Inial commi

---
+++ Method Tutorial/Firstm.java
@@ -0,0 +1,17 @@
+public class Firstm{
+    void add(){
+        int a=4;
+        int b=8;
+        int c=a+b;
+        System.out.println(c);
+    }
+    public static void main(String[] args){
+
+        Firstm a=new Firstm();
+        a.add();
+        
+       
+
+
+    }
+}
