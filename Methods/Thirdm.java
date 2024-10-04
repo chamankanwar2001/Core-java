78f7ea72e45eb413a0d99ed6f38dd5e1e6522903
chaman <chamanshekhawat712@gmail.com>
2024-10-04 07:04:00 +0530

    Initial commit

---
+++ Methods/Thirdm.java
@@ -0,0 +1,107 @@
+/*public class Thirdm {
+    int add(){
+        int a=2;
+        int b=4;
+        int c=a+b;
+        System.out.println(c);
+        return c;
+    }
+    int div(){
+        int a=2;
+        int b=8;
+        int c=a/b;
+        System.out.println(c);
+        return c;
+
+    }
+    int multi(){
+        int a=5;
+        int b=6;
+        int c=a*b;
+        System.out.println(c);
+        return c;
+    }
+    int sub(){
+        int a=6;
+        int b=3;
+        int c=a-b;
+        System.out.println(c);
+        return c;
+
+    }
+    int mod(){
+        int a=40;
+        int b=11;
+        int c=a%b;
+        System.out.println(c);
+        return c;
+    }
+    public static void main(String[] args){
+        Thirdm k=new Thirdm();
+        k.add();
+        k.div();
+        k.multi();
+        k.sub();
+        k.mod();
+
+
+
+
+
+    }
+    
+}*/
+class A{
+    int add(){
+        int a=3;
+        int b=5;
+        int c=a+b;
+        System.out.println(c);
+        return c;
+
+    }
+    int div(){
+        int a=2;
+        int b=8;
+        int c=a/b;
+        System.out.println(c);
+        return c;
+
+    }
+    int multi(){
+        int a=5;
+        int b=6;
+        int c=a*b;
+        System.out.println(c);
+        return c;
+    }
+    int sub(){
+        int a=6;
+        int b=3;
+        int c=a-b;
+        System.out.println(c);
+        return c;
+
+    }
+    int mod(){
+        int a=40;
+        int b=11;
+        int c=a%b;
+        System.out.println(c);
+        return c;
+    }
+}
+public class Thirdm{
+    public static void main(String[] args){
+        A k=new A();
+        k.add();
+        k.div();
+        k.multi();
+        k.sub();
+        k.mod();
+
+
+    }
+}
+
+
