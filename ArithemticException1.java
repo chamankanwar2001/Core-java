15855f3833648d46cb8403fd87f17dd6c5247a38
chaman <chamanshekhawat712@gmail.com>
2024-10-08 21:41:23 +0530

    Initial commit

--- ArithemticException1.java
+++ ArithemticException1.java
@@ -1,4 +1,4 @@
-public class ArithemticException1 {
+/*public class ArithemticException1 {
     public static void main(String[] args) {
         try {
             int a = 7;
@@ -8,6 +8,22 @@ public class ArithemticException1 {
         }
         catch (ArithmeticException ex) {
             System.out.println("exception handle");
+        }
+    }
+}*/
+
+
+
+public class ArithemticException1 {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            System.out.println(a / b);
+        } catch (ArithmeticException ex) {
+            System.out.println("Exception Handle");
+        } finally {
+            System.out.println("always executed");
         }
     }
 }
