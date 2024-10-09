6b7db5e35409a3114bb0946ced9a3dd3caf16e65
chaman <chamanshekhawat712@gmail.com>
2024-10-09 16:32:56 +0530

    Initial commit

---
+++ Exception_Handling/CreateThread1.java
@@ -0,0 +1,14 @@
+public class CreateThread1 {
+    
+        public void run(){
+        System.out.println("hello world");
+        }
+        public static void main(String[] args) {
+    
+            CreateThread1 t=new CreateThread1();
+             t.run();
+            
+        }
+        
+    }
+    
