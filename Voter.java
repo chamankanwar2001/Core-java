13af5711d85628fe66b316ef6ec317fd15fe6b50
chaman <chamanshekhawat712@gmail.com>
2024-10-16 18:49:50 +0530

    Initial commit

---
+++ Voter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Voter{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter your age :");
+        int age=sc.nextInt();
+        if(age >= 18){
+            System.out.println("you agr aligible for vote ");
+
+        }
+        else{
+            System.out.println("you are not aligible for vote ");
+
+        }
+
+    }
+    
+}
+
+
+    
+        
+        
+
+
+    
