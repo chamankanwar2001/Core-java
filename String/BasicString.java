7773a34730d6eeb86784c009ae79186df9c1e9b7
chaman <chamanshekhawat712@gmail.com>
2024-11-11 23:29:38 +0530

    Initial commit

---
+++ String/BasicString.java
@@ -0,0 +1,7 @@
+public class BasicString {
+    public static void main(String[] args){
+        String name = " Regex is  a programming language";
+        System.out.println(name);
+    }
+    
+}
