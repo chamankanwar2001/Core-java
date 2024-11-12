783d10d4f354858ce5060822578a0a725f914620
chaman <chamanshekhawat712@gmail.com>
2024-11-12 10:10:10 +0530

    Initial commit

---
+++ String/LengthOfString.java
@@ -0,0 +1,11 @@
+public class LengthOfString {
+    public static void main(String[] args) {
+        String name= "chaman";
+        System.out.println(name.length());
+        
+        int a[] ={1,2,3,4,5};
+        System.out.println(a.length);
+        
+    }
+    
+}
