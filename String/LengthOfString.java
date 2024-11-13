577dba18580b8b59431c1c7b7046e7081fad45ad
chaman <chamanshekhawat712@gmail.com>
2024-11-13 21:41:41 +0530

    Initial commit

--- String/LengthOfString.java
+++ String/LengthOfString.java
@@ -1,10 +1,12 @@
 public class LengthOfString {
     public static void main(String[] args) {
         String name= "chaman";
+        System.out.println(name);
         System.out.println(name.length());
         
-        int a[] ={1,2,3,4,5};
-        System.out.println(a.length);
+        String name1= "kanwar";
+        System.out.println(name1);
+        System.out.println(name1.length());
         
     }
     
