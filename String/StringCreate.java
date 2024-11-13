6344aa28225aab98870b52619a7b1cab3d4a8fc7
chaman <chamanshekhawat712@gmail.com>
2024-11-13 22:02:06 +0530

    String create constant and dynamic

---
+++ String/StringCreate.java
@@ -0,0 +1,11 @@
+public class StringCreate {
+    public static void main(String[] args) {
+        String name = "chaman";
+        System.out.println(name);
+        String name1= new String ("hello chaman");
+        System.out.println(name1);
+        
+
+    }
+    
+}
