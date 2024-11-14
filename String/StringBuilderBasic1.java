8211d63845011ed96e7741cb53ecb233ba56bce7
chaman <chamanshekhawat712@gmail.com>
2024-11-14 15:59:58 +0530

    String builder in string

---
+++ String/StringBuilderBasic1.java
@@ -0,0 +1,11 @@
+
+public class StringBuilderBasic1 {
+  public static void main(String[] args) {
+    StringBuilder sb=new StringBuilder("hello ");
+    sb.append(" world");
+    System.out.println(sb);
+
+  }
+
+    
+}
