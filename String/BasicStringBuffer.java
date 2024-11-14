17d382a713fdb3d50abd602d1642e980800e974e
chaman <chamanshekhawat712@gmail.com>
2024-11-14 15:36:50 +0530

    final error code in string buffer

---
+++ String/BasicStringBuffer.java
@@ -0,0 +1,12 @@
+public class BasicStringBuffer {
+    public static void main(String[] args) {
+        final StringBuffer sb=new StringBuffer("hello");
+        sb.append("world");
+        System.out.println(sb);
+        sb =sb.append("duniya");
+        System.out.println(sb);
+        // giver error final does nor reassign value
+        
+    }
+    
+}
