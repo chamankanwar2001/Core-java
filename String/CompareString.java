59f59f81ad813050c567b7883e473168eed63247
chaman <chamanshekhawat712@gmail.com>
2024-11-14 08:41:26 +0530

    Compare String in java string

---
+++ String/CompareString.java
@@ -0,0 +1,9 @@
+public class CompareString {
+    public static void main(String[] args) {
+        String p1=new String("chaman");
+        String p2=new String ("chaman");
+        System.out.println(p1==p2);
+        System.out.println(p1.equals(p2));
+    }
+    
+}
