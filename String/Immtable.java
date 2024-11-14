ed4f552cbe512da8f9aaca76631e06978d180aef
chaman <chamanshekhawat712@gmail.com>
2024-11-14 08:18:23 +0530

    Immutable in string

---
+++ String/Immtable.java
@@ -0,0 +1,9 @@
+public class Immtable {
+    public static void main(String[] args) {
+        String p= new String ("chaman");
+        System.out.println(p);
+        p.concat ("world");
+        System.out.println(p);
+    }
+    
+}
