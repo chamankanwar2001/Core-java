ec476234e886d8bd868e8256d7db309ddaf36c2a
chaman <chamanshekhawat712@gmail.com>
2024-11-15 09:05:13 +0530

    Initial commit

---
+++ String/StringBufferContructor.java
@@ -0,0 +1,9 @@
+
+public class StringBufferContructor {
+    public static void main(String[] args) {
+        StringBuffer sb=new StringBuffer ("hello");
+        String s=new String (sb);
+        System.out.println(s);
+    }
+    
+}
