f09290a766dcabe1e0a25cc23203aa07391c5803
chaman <chamanshekhawat712@gmail.com>
2024-11-15 09:11:27 +0530

    String char array cons in String

---
+++ String/StringCharArrayConstructor.java
@@ -0,0 +1,9 @@
+public class StringCharArrayConstructor {
+    public static void main(String[] args) {
+        char a[]= {'c', 'h','a'};
+        String s=new String(a);
+        System.out.println(s);
+
+    }
+    
+}
