a2e81cfc8913f6650d6716b65692d2c6f963e70d
chaman <chamanshekhawat712@gmail.com>
2024-11-15 08:55:45 +0530

    String literal in Stirng

---
+++ String/StringLiteralConstructer.java
@@ -0,0 +1,8 @@
+public class StringLiteralConstructer {
+    public static void main(String[] args) {
+        String name ="chaman kanwar";
+        String s=new String(name);
+        System.out.println(s);
+    }
+    
+}
