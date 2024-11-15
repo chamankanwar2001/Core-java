7997b45b853937105af3f7eac4aba4f9d20b5ee7
chaman <chamanshekhawat712@gmail.com>
2024-11-15 09:16:14 +0530

    String byte array const in String

---
+++ String/StringByteArrayConstructor.java
@@ -0,0 +1,8 @@
+public class StringByteArrayConstructor {
+    public static void main(String[] args) {
+        byte b[]= {2,45,6,78};
+        String s=new String(b);
+        System.out.println(s);
+    }
+    
+}
