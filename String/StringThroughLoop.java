694360a0e2050e8f0a1832342d4a5f184555cbf4
chaman <chamanshekhawat712@gmail.com>
2024-11-16 08:29:50 +0530

    String print through loop

---
+++ String/StringThroughLoop.java
@@ -0,0 +1,12 @@
+public class StringThroughLoop {
+    public static void main(String[] args) {
+        String s= "chaman";
+        char a[]= s.toCharArray();
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+
+        }
+        
+    }
+
+}
