0c62fd8d8a2b822b857d42627047492316e8bbed
chaman <chamanshekhawat712@gmail.com>
2024-11-16 08:36:48 +0530

    string length without length method

---
+++ String/CountOfLengthString.java
@@ -0,0 +1,14 @@
+public class CountOfLengthString {
+    public static void main(String[]args){
+        String s="hello world";
+        char a[] = s.toCharArray();
+        int count=0;
+        for(int i=0;i<a.length;i++){
+            count ++;
+
+        }
+        System.out.println(count);
+
+
+}
+}
