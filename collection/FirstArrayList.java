759fdbe215ff0217c9e90e88eb728ffd235988b6
chaman <chamanshekhawat712@gmail.com>
2024-11-26 06:35:31 +0530

    collection in java

---
+++ collection/FirstArrayList.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class FirstArrayList{
+    public static void main(String[] args) {
+        ArrayList al=new ArrayList();
+        al.add("chaman");
+        al.add("kanwar");
+        al.add(23);
+        al.add('c');
+
+        System.out.println(al);
+
+    }
+    
+}
