ce02dd57178139298dc296a00cd54ca333f770f9
chaman <chamanshekhawat712@gmail.com>
2024-11-26 06:42:31 +0530

    Initial commit

---
+++ collection/ArrayListCreat.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class ArrayListCreat {
+    public static void main(String[] args) {
+        ArrayList al=new ArrayList();
+        al.add("chaman");
+        al.add("kanwar");
+        al.add(23);
+        al.add('c');
+
+        System.out.println(al);
+
+    }
+    
+}
+    
+
