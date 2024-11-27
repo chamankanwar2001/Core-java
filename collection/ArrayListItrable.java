ae5579b79327c1e7280b7270d066de6867d25bbb
chaman <chamanshekhawat712@gmail.com>
2024-11-27 17:03:04 +0530

    Initial commit

---
+++ collection/ArrayListItrable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayListItrable {
+    public static void main(String[] args) {
+        ArrayList al=new ArrayList();
+        al.add("hello");
+        al.add(45);
+        al.add(4.5);
+        al.add('c');
+        System.out.println(al);
+        
+        
+    }
+    
+}
